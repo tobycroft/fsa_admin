2b8b7a6e0bf055a3a701068d2034f8c2ed33d4b6
火线兔 <silverhawk@foxmail.com>
2022-05-01 13:15:42 +0800

    TuuzPC

--- application/admin/controller/Menu.php
+++ application/admin/controller/Menu.php
@@ -197,7 +197,7 @@ class Menu extends Admin
         $info = MenuModel::get($id);
         // 拥有该节点权限的角色
         $info['role'] = RoleModel::getRoleWithMenu($id);
-
+print_r($info['role']);
         // 使用ZBuilder快速创建表单
         return ZBuilder::make('form')
             ->setPageTitle('编辑节点')
