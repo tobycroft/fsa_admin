83df4b842aef50fd4cff2ed5dc851430603e170d
火线兔 <silverhawk@foxmail.com>
2022-05-01 13:08:44 +0800

    TuuzPC

--- application/fsa/info.php
+++ application/fsa/info.php
@@ -6,7 +6,7 @@
  */
 return [
     // 模块名[必填]
-    'name'        => 'FamilyEducation',
+    'name'        => 'fsa',
     // 模块标题[必填]
     // 模块唯一标识[必填]，格式：模块名.开发者标识.module
     'identifier'  => 'fsa.fsa.module',
