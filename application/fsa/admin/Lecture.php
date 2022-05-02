e58ffd731368d8b76ff7ed8df0a24dbdd3b2c582
火线兔 <silverhawk@foxmail.com>
2022-05-02 16:02:48 +0800

    TuuzPC

--- application/fsa/admin/Lecture.php
+++ application/fsa/admin/Lecture.php
@@ -70,13 +70,13 @@ class Lecture extends Admin
 
         $btn_access = [
             'title' => '现场记录',
-            'icon' => 'fa fa-fw fa-key',
+            'icon' => 'fa fa-fw fa-bars',
 //            'class' => 'btn btn-xs btn-default ajax-get',
             'href' => url('lecture_record/index', ['search_field' => 'lid', 'keyword' => '__id__'])
         ];
         $btn_access1 = [
             'title' => '权限',
-            'icon' => 'fa fa-fw fa-key',
+            'icon' => 'fa fa-fw fa-minus-circle',
 //            'class' => 'btn btn-xs btn-default ajax-get',
             'href' => url('lecture_auth/index', ['search_field' => 'lid', 'keyword' => '__id__'])
         ];
