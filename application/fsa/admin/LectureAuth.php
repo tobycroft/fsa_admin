87ba679178ece536b1f24f8644fc65d8da7fce7a
火线兔 <silverhawk@foxmail.com>
2022-05-02 15:59:56 +0800

    TuuzPC

--- application/fsa/admin/LectureAuth.php
+++ application/fsa/admin/LectureAuth.php
@@ -54,7 +54,7 @@ class LectureAuth extends Admin
                 ["right_button", "功能"],
             ])
             ->addRightButtons(["edit" => "修改", "delete" => "删除",])
-//            ->addRightButton("custom", $btn_access)
+            ->addRightButton("custom", $btn_access)
             ->addTopButtons(["add" => "发帖"])
 //            ->setColumnWidth('title', 300)
             ->setRowList($data_list) // 设置表格数据
