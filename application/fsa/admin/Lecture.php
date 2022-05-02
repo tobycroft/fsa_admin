5dea05f672936b52ae010b082511d17cd4c635a2
火线兔 <silverhawk@foxmail.com>
2022-05-02 14:18:47 +0800

    TuuzPC

--- application/fsa/admin/Lecture.php
+++ application/fsa/admin/Lecture.php
@@ -82,7 +82,7 @@ class Lecture extends Admin
                 ["right_button", "功能"],
             ])
             ->addRightButtons(["edit" => "修改", "delete" => "删除",])
-            ->addRightButton("custom", $btn_access)
+//            ->addRightButton("custom", $btn_access)
             ->addTopButtons(["add" => "发帖"])
             ->setRowList($data_list) // 设置表格数据
             ->setPages($page)
