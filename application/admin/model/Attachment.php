48d8d46d0ed4dde7fa414f783f304c3962fbba4b
tobycroft <silverhawk@foxmail.com>
2022-05-27 17:02:12 +0800

    TuuzMiPro

--- application/admin/model/Attachment.php
+++ application/admin/model/Attachment.php
@@ -20,12 +20,12 @@ class Attachment extends Model
     /**
      * 根据附件id获取路径
      * @param string|array $id 附件id
-     * @param  int $type 类型：0-补全目录，1-直接返回数据库记录的地址
-     * @author 蔡伟明 <314013107@qq.com>
+     * @param int $type 类型：0-补全目录，1-直接返回数据库记录的地址
      * @return array|bool|mixed|string
      * @throws \think\db\exception\DataNotFoundException
      * @throws \think\db\exception\ModelNotFoundException
      * @throws \think\exception\DbException
+     * @author 蔡伟明 <314013107@qq.com>
      */
     public function getFilePath($id = '', $type = 0)
     {
@@ -34,7 +34,7 @@ class Attachment extends Model
             $paths = [];
             foreach ($data_list as $key => $value) {
                 if ($value['driver'] == 'local') {
-                    $paths[$value['id']] = ($type == 0 ? PUBLIC_PATH : '').$value['path'];
+                    $paths[$value['id']] = ($type == 0 ? PUBLIC_PATH : '') . $value['path'];
                 } else {
                     $paths[$value['id']] = $value['path'];
                 }
@@ -44,7 +44,7 @@ class Attachment extends Model
             $data = $this->where('id', $id)->find();
             if ($data) {
                 if ($data['driver'] == 'local') {
-                    return ($type == 0 ? PUBLIC_PATH : '').$data['path'];
+                    return ($type == 0 ? PUBLIC_PATH : '') . $data['path'];
                 } else {
                     return $data['path'];
                 }
@@ -57,18 +57,18 @@ class Attachment extends Model
     /**
      * 根据图片id获取缩略图路径，如果缩略图不存在，则返回原图路径
      * @param string $id 图片id
-     * @author 蔡伟明 <314013107@qq.com>
      * @return array|mixed|string|Model|null
      * @throws \think\db\exception\DataNotFoundException
      * @throws \think\db\exception\ModelNotFoundException
      * @throws \think\exception\DbException
+     * @author 蔡伟明 <314013107@qq.com>
      */
     public function getThumbPath($path = '')
     {
-        $result = $this->where('path', $path)->field('path,driver,thumb')->find();
+        $result = $this->where('path', $path)->whereOr('id', $path)->field('path,driver,thumb')->find();
         if ($result) {
             if ($result['driver'] == 'local') {
-                return $result['thumb'] != '' ? PUBLIC_PATH.$result['thumb'] : PUBLIC_PATH.$result['path'];
+                return $result['thumb'] != '' ? PUBLIC_PATH . $result['thumb'] : PUBLIC_PATH . $result['path'];
             } else {
                 return $result['thumb'] != '' ? $result['thumb'] : $result['path'];
             }
@@ -79,7 +79,7 @@ class Attachment extends Model
 
     /**
      * 根据附件id获取名称
-     * @param  string $id 附件id
+     * @param string $id 附件id
      * @return string     名称
      */
     public function getFileName($id = '')
