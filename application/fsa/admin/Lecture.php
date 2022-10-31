633f0d2b478803dc41ae6e1d033ee56d400d145b
火线兔 <silverhawk@foxmail.com>
2022-10-31 09:46:21 +0800

    TuuzPC

--- application/fsa/admin/Lecture.php
+++ application/fsa/admin/Lecture.php
@@ -214,7 +214,7 @@ class Lecture extends Admin
 
             $excel = new Excel(config("upload_prefix"));
             $ex = $excel->send_dp($data["file"]);
-            echo $ex->responsel;
+            echo $ex->response;
             var_dump($ex);
             exit();
 
