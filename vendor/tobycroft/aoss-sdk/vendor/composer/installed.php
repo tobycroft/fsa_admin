529b74f372bd9bfc5d38e6a07ca617d5f45b70ef
火线兔 <silverhawk@foxmail.com>
2023-04-14 13:24:56 +0800

    TuuzMac

---
+++ vendor/tobycroft/aoss-sdk/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'tobycroft/aoss-sdk',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '37490e88a60b5f0608d22f5c8e073538a2250dcd',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'tobycroft/aoss-sdk' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '37490e88a60b5f0608d22f5c8e073538a2250dcd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
