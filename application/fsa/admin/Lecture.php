93810c9de36cb8181006af91d9f388a47efda53d
tuuz <silverhawk@foxmail.com>
2023-09-16 12:59:17 +0800

    TuuzPC

--- application/fsa/admin/Lecture.php
+++ application/fsa/admin/Lecture.php
@@ -242,6 +242,11 @@ class Lecture extends Admin
             'icon' => 'fa fa-fw fa-key',
             'href' => url('upload2')
         ];
+        $export = [
+            'title' => '导出',
+            'icon' => 'fa fa-fw fa-key',
+            'href' => url('export')
+        ];
         $association = AssociationModel::column("id,name");
         return ZBuilder::make('table')
             ->addOrder('a.id')
@@ -283,7 +288,8 @@ class Lecture extends Admin
             ->addRightButton("custom", $btn_access1)
             ->addTopButtons(["add" => "发帖", "delete" => "删除"])
             ->addTopButton("upload", $top_upload)
-            ->addTopButton("upload", $top_upload2)
+            ->addTopButton("upload2", $top_upload2)
+            ->addTopButton("export", $export)
             ->setRowList($data_list) // 设置表格数据
             ->setPages($page)
             ->fetch();
