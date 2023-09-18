a2fb9633d53b7867218224f11883e563e91f37b7
火线兔 <silverhawk@foxmail.com>
2023-09-18 14:11:14 +0800

    TuuzMac

--- application/fsa/action/LectureAction.php
+++ application/fsa/action/LectureAction.php
@@ -8,6 +8,7 @@ use app\fsa\model\InstructorModel;
 use app\fsa\model\LectureModel;
 use app\fsa\model\TagDataunitModel;
 use app\fsa\model\TagFormModel;
+use app\fsa\model\TagModel;
 use app\fsa\model\TagRoleModel;
 
 class LectureAction
@@ -194,7 +195,6 @@ class LectureAction
 
     protected function jinjiangfix($excel)
     {
-
         foreach ($excel as $value) {
             $StartDate = $value['活动开始时间'];
             $Visitor = $value['参与人数'];
@@ -295,6 +295,100 @@ class LectureAction
 
     protected function jinjiangnew($excel)
     {
+        foreach ($excel as $value) {
+            $id = $value['id'];
+            $start_date = $value['讲座开始时间'];
+            $Visitor = $value['学员人数'];
+            $duration = $value['时长(秒)'];
+            $tr_name = $value['角色标签'];
+            $tf_name = $value['形式标签'];
+            $phone = $value['手机号码'];
+            $title = $value['讲座主题'];
+            $type = $value['活动类别'];
+            //search in type check contain some characters
+            if (str_contains($type, '线上') && str_contains($type, '线下')) {
+                $type = '线上与线下';
+            } elseif (str_contains($type, '线上')) {
+                $type = '线上';
+            } elseif (str_contains($type, '线下')) {
+                $type = '线下';
+            } else {
+                throw new \Error('活动类型需要填写线上或线下');
+            }
 
+            $instructor = $value['讲师'];
+            $host = $value["主办方"];
+            $dataunits = explode(',', $value['标签数据归属方ids']);
+            $tags = $value['标签ids'];
+            $City = $value['市'];
+            $Province = $value['省'];
+            $District = $value['区'];
+            $Street = $value['街道'];
+
+            if (strlen($phone) < 5) {
+                throw new \Error('手机号不能为空');
+            }
+            if (strlen($instructor) < 1) {
+                throw new \Error('姓名不能为空');
+            }
+            if (strlen($Province) < 1) {
+                throw new \Error('省不能为空');
+            }
+            if (strlen($City) < 1) {
+                throw new \Error('城市不能为空');
+            }
+            if (strlen($District) < 1) {
+                throw new \Error('乡镇区不能为空');
+            }
+            $tag_dataunit_ids = TagDataunitModel::whereIn('name', $dataunits)
+                ->where('aid', $this->association->id)
+                ->column('id');
+            $tag_ids = TagModel::whereIn('name', $tags)
+                ->where('aid', $this->association->id)
+                ->column('id');
+
+            $tag_role_ids = TagRoleModel::whereIn('name', [$tr_name])
+                ->where('aid', $this->association->id)
+                ->column('id');
+            if (empty($tag_role_ids)) {
+                TagRoleModel::create([
+                    'aid' => $this->association->id,
+                    'is_show' => 1,
+                    'name' => $tr_name,
+                ]);
+            }
+            $tag_form_ids = TagFormModel::where('name', $tf_name)
+                ->where('aid', $this->association->id)
+                ->column('id');
+            if (empty($tag_form_ids)) {
+                TagFormModel::create([
+                    'aid' => $this->association->id,
+                    'is_show' => 1,
+                    'name' => $tf_name,
+                ]);
+            }
+            $lecture = LectureModel::where('aid', $this->association->id)
+                ->where('id', $id)
+                ->find();
+            if ($lecture) {
+                LectureModel::where('id', $lecture->id)
+                    ->data([
+                        'tag_dataunit_ids' => implode(',', $tag_dataunit_ids),
+                        'tag_ids' => implode(',', $tag_ids),
+                        'trid' => implode(',', $tag_role_ids),
+                        'tfid' => implode(',', $tag_form_ids),
+                        'start_date' => $start_date,
+                        'type' => $type,
+                        'province' => $Province,
+                        'city' => $City,
+                        'district' => $District,
+                        'street' => $Street,
+                        'visitor' => $Visitor,
+                    ])
+                    ->update();
+            } else {
+
+            }
+        }
     }
 }
