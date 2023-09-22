fb83ba6ff66044dddb362e2008f96051b71bdb57
火线兔 <silverhawk@foxmail.com>
2023-09-22 11:10:46 +0800

    TuuzMac

--- application/fsa/action/LectureAction.php
+++ application/fsa/action/LectureAction.php
@@ -83,6 +83,9 @@ class LectureAction
             if (strlen($StartDate) < 5) {
                 throw new \Error("开始时间为空-行数：" . $i);
             }
+            if (strlen($Visitor) < 5) {
+                throw new \Error("参数人数不能为空-行数：" . $i);
+            }
             if (strlen($phone) < 5) {
                 throw new \Error("手机号-行数：" . $i);
             }
